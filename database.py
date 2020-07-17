23818f5f30dd3fc7b0c79d31b18cf0e5c09e4d5b
mowshon <vasile.buldumac@ati.utm.md>
2020-07-17 17:40:41 +0300

    Initial database structure

---
+++ database.py
@@ -0,0 +1,37 @@
+from peewee import SqliteDatabase, Model
+from peewee import IntegerField, CharField, PrimaryKeyField, TimestampField, TextField
+from pathlib import Path
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read("config.ini", encoding="utf-8")
+
+db = SqliteDatabase(Path.cwd() / config.get('database', 'file'))
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Question(BaseModel):
+    id = PrimaryKeyField(null=False)
+    # Заголовок вопроса
+    title = CharField()
+    # Последний MD5 из содержимого вопроса и кода (если есть)
+    last_checksum = CharField()
+    # Путь к JSON файлу вопроса
+    json_file = CharField()
+    # Путь к сгенерированной изображении с кодом
+    image = CharField()
+    # ID сообщения в Telegram
+    message_id = IntegerField()
+    created_on = TimestampField()
+
+    class Meta:
+        db_table = 'questions'
+
+
+db.connect()
+if not Question.table_exists():
+    Question.create_table()
